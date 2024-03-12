dd10aa97d97489d0a2d4c751531c0e6e97891c2f
Angel Perez Personal <pasaperez@yahoo.com.ar>
2024-03-12 17:08:24 -0300

    replace ui

--- scripts/ui.js
+++ scripts/ui.js
@@ -3,27 +3,11 @@
  ****************************************************/
 
 exports.openInNewTab = function (url) {
-    sys.logs.info('[ui] Open in new tab');
-    var userId = sys.context.getCurrentUserRecord().id();
-    sys.logs.info('[ui] User id: '+JSON.stringify(userId));
-    sys.ui.sendMessage({
-        scope: 'uiService:slingr.uiManagement',
-        name: 'openInNewTab',
-        target: 'users',
-        targetUsers: [userId],
-        url: url,
-    });
+    sys.logs.info('[slingr] Open in new tab');
+    dependencies.utils.ui.openInNewTab(url);
 }
 
 exports.openInCurrentTab = function (url) {
-    sys.logs.info('[ui] Open in new tab');
-    var userId = sys.context.getCurrentUserRecord().id();
-    sys.logs.info('[ui] User id: '+JSON.stringify(userId));
-    sys.ui.sendMessage({
-        scope: 'uiService:uiManagement',
-        name: 'openInCurrentTab',
-        target: 'users',
-        targetUsers: [userId],
-        url: url,
-    });
+    sys.logs.info('[slingr] Open in current tab');
+    dependencies.utils.ui.openInCurrentTab(url);
 }
--- uiServices/uiManagement/uiService.js
+++
@@ -1,9 +0,0 @@
-service.openInNewTab = function (message) {
-    var url = message.url;
-    window.open(url, '_blank').focus();
-}
-
-service.openInCurrentTab = function (message) {
-    var url = message.url;
-    window.open(url, '_self').focus();
-}
