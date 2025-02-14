57b365dd0a2e123a62133c46e85728c3df14fb0c
Angel Perez Personal <pasaperez@yahoo.com.ar>
2025-02-14 15:21:13 -0300

    Package configurationBuilder.js

--- configurationBuilder.js
+++ configurationBuilder.js
@@ -6,17 +6,5 @@
  ****************************************************/
 
 let configurationBuilder = function (config) {
-    if (config.authenticationMethod === 'oAuth2') {
-        config.oauth = {
-            id: 'installationInfo-googleslides-User-'+sys.context.getCurrentUserRecord().id(),
-            authUrl: 'https://accounts.google.com/o/oauth2/auth',
-            accessTokenUrl: 'https://oauth2.googleapis.com/token',
-            clientId: config.clientId,
-            clientSecret: config.clientSecret,
-            scope: config.scope,
-            state: config.state,
-            oauthCallback: config.oauthCallback
-        };
-    }
     return config;
 }
